d2d20f015c470b846525c470829c199f6a86442a
Selina Su <phongsu20@gmail.com>
2023-02-03 14:04:50 -0800

    backup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
-//require('dotenv').config();
 
-//mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/movieDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
