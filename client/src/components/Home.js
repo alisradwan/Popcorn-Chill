1f94e601f405e176a1be495b03c4e1880c13b1fe
SaintMartyrn <dartagnanhickey@.com>
2023-02-06 18:28:24 -0800

    cleaning code

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -153,18 +153,7 @@ function Home() {
               Your #1 Movie Database
             </h3>
           </div>
-          {/* <div class="input-group rounded">
-            <input
-              type="search"
-              class="form-control rounded"
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="search-addon"
-            />
-            <span class="input-group-text border-0" id="search-addon">
-              <i class="fas fa-search"></i>
-            </span>
-          </div> */}
+
           <Container>
             <Form
               className="input-group rounded"
@@ -213,8 +202,6 @@ function Home() {
           <div class="row text-center">
             <div class="col-md-4">
               <span class="fa-stack fa-4x">
-                {/* <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i> */}
               </span>
               <h4 class="my-3">Search for Endless Movies</h4>
               <p class="text-muted">
@@ -225,8 +212,6 @@ function Home() {
             </div>
             <div class="col-md-4">
               <span class="fa-stack fa-4x">
-                {/* <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                < class="fa-duotone fa-film fa-stack-1x fa-inverse"></i> */}
               </span>
               <h4 class="my-3">Create a profile to save your favorites</h4>
               <p class="text-muted">
@@ -237,8 +222,6 @@ function Home() {
             </div>
             <div class="col-md-4">
               <span class="fa-stack fa-4x">
-                {/* <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-lock fa-stack-1x fa-inverse"></i> */}
               </span>
               <h4 class="my-3">See what people are watching now</h4>
               <p class="text-muted">
@@ -250,7 +233,7 @@ function Home() {
           </div>
         </div>
       </section>
-      {/* <!-- Portfolio Grid--> */}
+
       <section class="page-section bg-light" id="portfolio">
         <video
           src={require("../assets/VideoTp.mp4")}
@@ -259,7 +242,7 @@ function Home() {
           muted
         ></video>
       </section>
-      {/* <!-- About--> */}
+
       <section class="page-section" id="about">
         <div class="container">
           <div class="text-center">
@@ -378,7 +361,7 @@ function Home() {
           </ul>
         </div>
       </section>
-      {/* <!-- Team--> */}
+
       <section class="page-section bg-light" id="team">
         <div class="container">
           <div class="text-center">
@@ -392,7 +375,7 @@ function Home() {
             <div class="col-md-3">
               <div class="team-member">
                 <h4>Selina Su</h4>
-                <p class="text-muted">insert title</p>
+                <p class="text-muted">FullStack Developer</p>
                 <a
                   class="btn btn-dark btn-social mx-2"
                   href="https://github.com/fuuko08"
@@ -400,19 +383,12 @@ function Home() {
                 >
                   <i class="fab fa-github"></i>
                 </a>
-                <a
-                  class="btn btn-dark btn-social mx-2"
-                  href="#!"
-                  aria-label="Parveen Anand LinkedIn Profile"
-                >
-                  <i class="fab fa-linkedin-in"></i>
-                </a>
               </div>
             </div>
             <div class="col-md-3">
               <div class="team-member">
                 <h4>Ali Radwan</h4>
-                <p class="text-muted">insert title</p>
+                <p class="text-muted">FullStack Developer</p>
                 <a
                   class="btn btn-dark btn-social mx-2"
                   href="https://github.com/alisradwan"
@@ -420,19 +396,12 @@ function Home() {
                 >
                   <i class="fab fa-github"></i>
                 </a>
-                <a
-                  class="btn btn-dark btn-social mx-2"
-                  href="#!"
-                  aria-label="Diana Petersen LinkedIn Profile"
-                >
-                  <i class="fab fa-linkedin-in"></i>
-                </a>
               </div>
             </div>
             <div class="col-md-3">
               <div class="team-member">
                 <h4>D'Artagnan Hickey</h4>
-                <p class="text-muted">insert title</p>
+                <p class="text-muted">FullStack Developer</p>
                 <a
                   class="btn btn-dark btn-social mx-2"
                   href="https://github.com/SaintMartyrn"
@@ -440,19 +409,12 @@ function Home() {
                 >
                   <i class="fab fa-github"></i>
                 </a>
-                <a
-                  class="btn btn-dark btn-social mx-2"
-                  href="#!"
-                  aria-label="Larry Parker LinkedIn Profile"
-                >
-                  <i class="fab fa-linkedin-in"></i>
-                </a>
               </div>
             </div>
             <div class="col-md-3">
               <div class="team-member">
                 <h4>Aram Ambartsumyan</h4>
-                <p class="text-muted">insert title</p>
+                <p class="text-muted">FullStack Developer</p>
                 <a
                   class="btn btn-dark btn-social mx-2"
                   href="https://github.com/AramA89"
@@ -460,20 +422,13 @@ function Home() {
                 >
                   <i class="fab fa-github"></i>
                 </a>
-                <a
-                  class="btn btn-dark btn-social mx-2"
-                  href="#!"
-                  aria-label="Larry Parker LinkedIn Profile"
-                >
-                  <i class="fab fa-linkedin-in"></i>
-                </a>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col-lg-8 mx-auto text-center">
               <p class="large text-muted">
-                {" "}
+                
                 We are a group of fullstack developers from the 2023 UCLA Coding
                 Bootcamp. Front end, back end, we can do it all!
               </p>
@@ -481,11 +436,11 @@ function Home() {
           </div>
         </div>
       </section>
-      {/* <!-- Clients--> */}
+
       <div class="py-5">
         <div class="container"></div>
       </div>
-      {/* <!-- Contact--> */}
+
       <video src={require("../assets/VideoBg.mp4")} autoPlay loop muted></video>
     </>
   );
